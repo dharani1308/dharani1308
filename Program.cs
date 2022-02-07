da1d5f971c62e6a05589a96c8532016fd4c42883
dharani1308 <99172258+dharani1308@users.noreply.github.com>
2022-02-07 22:41:52 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assaignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Temperature in Celsius");
+            double Celsius = Convert.ToDouble(Console.ReadLine());
+            double Kelvins = Celsius + 273;
+            double Fahrenheit = Celsius * 18 / 10 + 32;
+            Console.WriteLine(Celsius+" Celsius = "+Kelvins+"K");
+            Console.WriteLine(Celsius+" Celsius = " +Fahrenheit+"F");
+            Console.ReadLine();
+
+        }
+    }
+}
